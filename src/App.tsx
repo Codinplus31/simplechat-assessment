2f7079fc3aeca5d216fed1e405e33d3f9f530829
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-01-02 18:24:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import axios from 'axios';
 import io from 'socket.io-client';
 import Login from './components/Login';
 import Chat from './components/Chat';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = 'https://simplechat-backend.vercel.app';
 const socket = io(API_URL);
 
 function App() {
