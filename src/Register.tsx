56217ede8e7d3c0423cbd5fb9ccebe5b8063dac7
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-01-02 23:22:02 +0100

    Update Register.tsx

--- src/Register.tsx
+++ src/Register.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = 'https://simplechat-backend.vercel.app';
+const API_URL = 'https://simplechat-backend-f4w5.onrender.com';
 
 function Register() {
   const [username, setUsername] = useState('');
