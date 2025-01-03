3f065e4250b7b9dc3756c143f6d07dcc560408be
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-01-03 20:57:55 +0100

    Update Chat.tsx

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -134,7 +134,7 @@ return e;
         recipientId: selectedUser.id
       });
 setIsTyping(true)
-      alert(`first ${user.id} sec ${selectedUser}`)
+      alert(`first ${user.id} sec ${selectedUser.id}`)
       // Clear existing timeout
       if (typingTimeoutRef.current) {
         window.clearTimeout(typingTimeoutRef.current);
