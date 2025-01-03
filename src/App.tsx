c1a64c7c2466aae4ba9b6e33a3e4603b7bdc688c
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-01-03 01:32:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import Register from './Register';
 import Chat from './Chat';
+import UserList from './UserList';
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/chat" element={<Chat />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/chat/:userId" element={<Chat />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </Router>
